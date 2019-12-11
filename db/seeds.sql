c5d91c4578f8826c34a22556b8575b0c5170d6bc
Ciprien Bossut <chefchino@live.com>
2019-12-11 12:33:32 -0500

    made basic structure for app, needs alot of debugging

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name) VALUES ("Smokehouse Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheddar Burger");
+INSERT INTO burgers (burger_name) VALUES ("Grilled Chicken Avocado Burger");
+INSERT INTO burgers (burger_name) VALUES ("Lambo");
+INSERT INTO burgers (burger_name) VALUES ("Grilled Mahi Burger");
+INSERT INTO burgers (burger_name) VALUES ("Sloppy Joe");
+INSERT INTO burgers (burger_name) VALUES ("ChesseBurger");
+INSERT INTO burgers (burger_name) VALUES ("Burger");
+INSERT INTO burgers (burger_name) VALUES ("Eggplant Sausage Burger");
+INSERT INTO burgers (burger_name) VALUES ("Fried Avocado Burger");
---
+++ public/assets/js/burger.js
@@ -0,0 +1,36 @@
+$(function() {
+$(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var devoured = $(this).data("devoured");
+    var newAte = {
+      devoured: devoured
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newAte
+    }).then(
+      function() {
+        console.log("You Devoured it");
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+    var newBurger = {
+      name: $("#ca").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+});
