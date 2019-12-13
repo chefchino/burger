8252a170240d096b3d66b407a5bb7d6332dfc32a
Ciprien Bossut <chefchino@live.com>
2019-12-13 00:23:11 -0500

    sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var connection; 
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(JAWSDB_URL)
-} else {
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(JAWSDB_URL)
+// } else {
 connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,7 +11,7 @@ connection = mysql.createConnection({
     password: "Snoogins1!",
     database: "burger_db"
 });
-};
+
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
